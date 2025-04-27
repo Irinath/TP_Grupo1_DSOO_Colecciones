562a9dabfcc47018ed5bfe099c8d847610286a0c
elavincho <elavincho@gmail.com>
2025-04-27 18:21:39 -0300

    Agrego clase test

--- Biblioteca/Lector.cs
+++ Biblioteca/Lector.cs
@@ -8,7 +8,67 @@ namespace Biblioteca
 {
     internal class Lector
     {
-        string nombre = "";
+        /* Atributos*/
+        private string nombre;
+        private string dni;
+        private List<Libro> prestamos;
+
+        /* Constructor Vacio*/
+        public Lector()
+        {
+        }
+
+        /* Constructor con parámetros*/
+        public Lector(string nombre, string dni)
+        {
+            this.nombre = nombre;
+            this.dni = dni;
+            prestamos = new List<Libro>();
+        }
+
+        /* Getters y Setters*/
+
+        public string getNombre()
+        {
+            return nombre;
+        }
+
+        public string getDni()
+        {
+            return dni;
+        }
+
+        public void setNombre()
+        {
+            this.nombre = nombre;
+        }
+
+        public void setDni()
+        {
+            this.dni = dni;
+        }
+
+        /* To String*/
+
+        public override string ToString()
+        {
+            return "Nombre: " + nombre + " DNI: " + dni + " Prestamos: " + prestamos.Count();
+        }
+
+        /* Métodos*/
+
+        public bool puedeTomarPrestado()
+        {
+            return prestamos.Count < 3;
+        }
+
+        public void agregarPrestamo(Libro libro)
+        {
+            if (puedeTomarPrestado())
+            {
+                prestamos.Add(libro);
+            }
+        }
 
     }
 }
